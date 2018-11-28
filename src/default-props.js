c6d1b14f2d95999d83abb88519b6b49bcb67cfed
Jacob Bare <jacob@limit0.io>
2018-11-27 21:11:27 -0600

    Add ClickEvent and OpenEvent defaults

--- src/default-props.js
+++ src/default-props.js
@@ -1,4 +1,23 @@
 module.exports = {
+  ClickEvent: [
+    'ObjectID',
+    'ID',
+    'BatchID',
+    'CreatedDate',
+    'EventDate',
+    'EventType',
+    'SendID',
+    'SubscriberKey',
+    'URL',
+    'URLID',
+  ],
+  OpenEvent: [
+    'ObjectID',
+    'EventDate',
+    'EventType',
+    'SendID',
+    'SubscriberKey',
+  ],
   SendClassification: [
     'ObjectID',
     'CustomerKey',
